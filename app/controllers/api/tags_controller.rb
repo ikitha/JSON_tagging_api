54de68c10d36bb2716f3a0170aa8c44231c31b11
Myla Baker <myla.baker@gmail.com>
2016-03-26 19:28:11 -0700

    establish create action

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -1,5 +1,6 @@
 module API
   class TagsController < ApplicationController
+    before_action :find_entity, only: [:entity, :entity_stats, :destroy]
     # get 'stats/:type/:identifier', to: 'tags#entity_stats'
     # get 'tags/:type/:identifier', to: 'tags#entity'
     # get '/stats', to: 'tags#stats'
@@ -7,6 +8,15 @@ module API
     # delete 'tags/:type/:identifier', to: 'tags#destroy'
 
     def create
+        @entity = Entity.create(entity_type: tag_params[:entity_type], identifier: tag_params[:entity_identifier])
+        @entity.create_tags(tag_params[:tags])
+        respond_to do |format|
+          format.json { render json: { entity_type: @entity.entity_type, entity_identifier: @entity.identifier, tags: @entity.tags }, status: :created }
+        end
+    rescue => e
+      respond_to do |format|
+        format.json { render json: e.message, status: :unprocessible_entity }
+      end
     end
 
     def entity
@@ -19,6 +29,22 @@ module API
     end
 
     def destroy
+    end
+
+  private
+    # white list params
+    def tag_params
+      params.permit(:entity_type, :entity_identifier, tags: [])
+    end
+
+    def find_entity
+      unless @entity = Entity.find_by(entity_type: tag_params[:entity_type], identifier: tag_params[:entity_identifier])
+        record_not_found
+      end
+    end
+
+    def record_not_found
+      render text: "404 - Record Not Found", status: :not_found
     end
   end
 end
--- app/models/entity.rb
+++ app/models/entity.rb
@@ -3,4 +3,11 @@ class Entity < ActiveRecord::Base
   validates :entity_type, presence: true
   validates :identifier, presence: true
   accepts_nested_attributes_for :tags
+
+  #create tags for the entity unless that tag already exists
+  def create_tags(tags)
+    tags.each do |tag_name|
+      self.tags.create!(name: tag_name) unless self.tags.find_by(name: tag_name)
+    end
+  end
 end
