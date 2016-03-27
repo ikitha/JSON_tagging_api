cb81bbb925070563e6f2529778b9dbad165cfd2f
Myla Baker <myla.baker@gmail.com>
2016-03-26 18:49:28 -0700

    Controller skeleton, api inflection

---
+++ app/controllers/api/tags_controller.rb
@@ -0,0 +1,24 @@
+module API
+  class TagsController < ApplicationController
+    # get 'stats/:type/:identifier', to: 'tags#entity_stats'
+    # get 'tags/:type/:identifier', to: 'tags#entity'
+    # get '/stats', to: 'tags#stats'
+    # post 'tags', to: 'tags#create'
+    # delete 'tags/:type/:identifier', to: 'tags#destroy'
+
+    def create
+    end
+
+    def entity
+    end
+
+    def entity_stats
+    end
+
+    def stats
+    end
+
+    def destroy
+    end
+  end
+end
