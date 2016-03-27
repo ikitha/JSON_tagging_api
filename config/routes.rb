94d22efa017dcd1636151de48b3887c466a69344
Myla Baker <myla.baker@gmail.com>
2016-03-26 18:44:30 -0700

    Set up routes under api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-
+# GET /stats/:entity_type/:entity_id
+# GET /stats
+# DELETE /tags/:entity_type/:entity_id
+# GET /tags/:entity_type/:entity_id
+# POST /tag
+  namespace :api, path: 'api/', defaults: { format: 'json' } do
+    get 'stats/:entity_type/:entity_identifier', to: 'tags#entity_stats'
+    get 'tags/:entity_type/:entity_identifier', to: 'tags#entity'
+    get 'stats', to: 'tags#stats'
+    post 'tags', to: 'tags#create'
+    delete 'tags/:entity_type/:entity_identifier', to: 'tags#destroy'
+  end
 end
